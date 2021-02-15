25fe2152f209e38dc74b19368f34a3f1abd371b4
EKaxada <enocksdha@gmail.com>
2021-02-15 15:56:17 +0300

    setup config keys and home route

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/api', (req, res) => {
-  res.status(200).json({api: 'version 1'})
-})
+app.use("/", (req, res) => {
+  res.status(200).json({ api: "version 1" });
+});
 
 // Database Connection URL
-const db = "mongodb+srv://EKaxada:1234567890@cluster0.3bhqr.mongodb.net/EKaxada?retryWrites=true&w=majority";
+const db = process.env.MONGO_URI || "mongodb://localhost:27017/homeschoolAPI";
 // Use connect method to connect to the server
 
-mongoose.connect(db).then(()=>console.log("db connected ..")).catch(err=>console.log(err))
+mongoose
+  .connect(db)
+  .then(() => console.log("db connected .."))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 8000;
 app.listen(port, function onStart(err) {
